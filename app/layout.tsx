5d97bc074a8634b8d455d20dbfdc94c1c9bb94b0
Tushar Gupta <tushar021508@gmail.com>
2025-02-16 01:59:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,9 @@ import { ClerkProvider } from "@clerk/nextjs";
 const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Discord Clone",
+  title: "Chat Fusion",
   description:
-    "Discord Clone with Next.js, React.js, TailWindCSS & TypeScript."
+    "Chat Fusion with Next.js, React.js, TailWindCSS & TypeScript."
 };
 
 export default function RootLayout({
